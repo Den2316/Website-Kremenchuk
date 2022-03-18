a50f81ff091017985b025e36efc693adf2199410
Denys Brashchenko <artetaden2316@gmail.com>
2022-03-18 12:40:53 +0200

    Added CSS for first section

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+const signInButton = document.querySelector('.sign-in-button')
+const hidden = document.querySelector('.hidden')
+const signIn = document.querySelector('.sign-in')
+const closeSignIn = document.querySelector('.close-sign-in')
+signInButton.addEventListener('click',function(){
+  signIn.classList.remove('hidden');
+  closeSignIn.classList.remove('hidden');
+
+})
+closeSignIn.addEventListener('click',function(){
+    signIn.classList.add('hidden');
+    closeSignIn.classList.remove('hidden');
+})
